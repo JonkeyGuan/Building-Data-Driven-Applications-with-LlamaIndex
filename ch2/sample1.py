760a6afb6ca71dcc8b269b48111a445768ba1ff4
Jonkey Guan <jonkey.guan@gmail.com>
2025-07-26 22:10:46 +0800

    changed for gemini

--- ch2/sample1.py
+++ ch2/sample1.py
@@ -1,3 +1,28 @@
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+
+llm = GoogleGenAI(
+    model="gemini-2.0-flash",
+)
+
+embed_model = GoogleGenAIEmbedding(model_name="models/embedding-001")
+
+documents = SimpleDirectoryReader('files').load_data()
+
+index = VectorStoreIndex.from_documents(
+    documents,
+    embed_model=embed_model
+)
+
+query_engine = index.as_query_engine(llm=llm)
+
+response = query_engine.query("summarize each document in a few sentences")
+print(response)
+
+
+""" 
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 documents = SimpleDirectoryReader('files').load_data()
@@ -6,3 +31,4 @@ query_engine = index.as_query_engine()
 response = query_engine.query("summarize each document in a few sentences")
 
 print(response)
+ """
